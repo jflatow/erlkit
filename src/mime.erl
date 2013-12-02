41a3114533f7d8f4832c5804df69167ecb774484
Jared Flatow <jflatow@gmail.com>
2013-12-02 14:49:18 -0800

    mime: fix up formatting

--- src/mime.erl
+++ src/mime.erl
@@ -7,26 +7,40 @@
          format/1,
          format/2]).
 
--export([content_type/1,
+-export([boundary/1,
+         boundary/2,
+         header/2,
+         header/3,
+         content_type/1,
          content_transfer_encoding/1,
          decode/1,
          decode/2,
          encode/1,
-         encode/2,
+         encode/2]).
+
+-export([skip_spaces/1,
+         wrap_lines/3,
          split_headers/1,
          split_parts/1]).
 
--define(CRLF, "\r\n").
--define(Q, "\"").
--define(IS_NONPRINTABLE(C), (C < 32 orelse
-                             C >= 127 orelse
-                             C =:= $= orelse
-                             C =:= $.)).
+-import(util, [bin/1,
+               hexdigit/1,
+               unhexdigit/1]).
 
-lower(Bin) when is_binary(Bin) ->
-    lower(binary_to_list(Bin));
-lower(Str) when is_list(Str) ->
-    string:to_lower(Str).
+-define(TAB, $\t).
+-define(SP, $\s).
+-define(CR, $\r).
+-define(LF, $\n).
+-define(CRLF, "\r\n").
+-define(Q, $\").
+-define(IS_PRINTABLE(C), (C >= ?SP andalso C =< $~ andalso C =/= $=)).
+
+normalize(Name) when is_atom(Name) ->
+    normalize(atom_to_list(Name));
+normalize(Name) when is_binary(Name) ->
+    normalize(binary_to_list(Name));
+normalize(Name) ->
+    string:to_lower(Name).
 
 read_quoted(<<?Q, Rest/binary>>) ->
     read_quoted(Rest, <<>>).
@@ -43,8 +57,44 @@ skip_spaces(<<C, Rest/binary>>) when C >= 9, C =< 13; C =:= 32 ->
 skip_spaces(Binary) ->
     Binary.
 
+wrap_lines(Data, Every, Break) ->
+    wrap_lines(Data, Every, Break, <<>>).
+
+wrap_lines(Data, Every, Break, Acc) when size(Data) > Every ->
+    <<Buf:Every/binary, Rest/binary>> = Data,
+    wrap_lines(Rest, Every, Break, <<Acc/binary, Buf/binary, Break/binary>>);
+wrap_lines(Data, _, Break, Acc) ->
+    <<Acc/binary, Data/binary, Break/binary>>.
+
+boundary(Headers) ->
+    boundary(Headers, error).
+
+boundary(Headers, Default) ->
+    case content_type(Headers) of
+        {<<"multipart/",  _/binary>>, Params} ->
+            proplists:get_value("boundary", Params, Default);
+        {_Type, _Params} ->
+            error
+    end.
+
+header(Headers, Name) ->
+    header(Headers, Name, undefined).
+
+header(Headers, Name, Default) ->
+    header(Headers, normalize(Name), Default, norm).
+
+header([{Field, Value}|Headers], Name, Default, norm) ->
+    case normalize(Field) of
+        Name ->
+            Value;
+        _ ->
+            header(Headers, Name, Default, norm)
+    end;
+header([], _, Default, norm) ->
+    Default.
+
 content_type(Headers) ->
-    content_type(proplists:get_value("content-type", Headers), undefined, []).
+    content_type(header(Headers, "content-type"), undefined, []).
 
 content_type(undefined, undefined, []) ->
     {"text/plain", []};
@@ -60,14 +110,14 @@ content_type([Param], Type, Params) ->
         [Attribute, <<?Q, _/binary>> = Rest0] ->
             [Value,Rest1] = read_quoted(Rest0),
             [_Xtra|Rest2] = binary:split(Rest1, <<";">>),
-            content_type(Rest2, Type, [{lower(Attribute), Value}|Params]);
+            content_type(Rest2, Type, [{normalize(Attribute), Value}|Params]);
         [Attribute, Rest0] ->
             [Value|Rest1] = binary:split(Rest0, <<";">>),
-            content_type(Rest1, Type, [{lower(Attribute), Value}|Params])
+            content_type(Rest1, Type, [{normalize(Attribute), Value}|Params])
     end.
 
 content_transfer_encoding(Headers) ->
-    lower(skip_spaces(proplists:get_value("content-transfer-encoding", Headers, "7bit"))).
+    normalize(skip_spaces(header(Headers, "content-transfer-encoding", "7bit"))).
 
 decode({Headers, Body}) ->
     decode(content_transfer_encoding(Headers), Body).
@@ -87,7 +137,7 @@ decode(_, Body) ->
 decode("quoted-printable", <<"=", ?CRLF, Rest/binary>>, Acc) ->
     decode("quoted-printable", Rest, Acc);
 decode("quoted-printable", <<"=", H1, H2, Rest/binary>>, Acc) ->
-    decode("quoted-printable", Rest, <<Acc/binary, (util:unhexdigit(H1) * 16 + util:unhexdigit(H2))>>);
+    decode("quoted-printable", Rest, <<Acc/binary, (unhexdigit(H1) * 16 + unhexdigit(H2))>>);
 decode("quoted-printable", <<C, Rest/binary>>, Acc) ->
     decode("quoted-printable", Rest, <<Acc/binary, C>>);
 decode("quoted-printable", <<>>, Acc) ->
@@ -97,19 +147,31 @@ encode({Headers, Body}) ->
     encode(content_transfer_encoding(Headers), Body).
 
 encode("base64", Body) ->
-    base64:encode(Body);
+    wrap_lines(base64:encode(Body), 76, <<?CRLF>>);
 encode("quoted-printable", Body) ->
-    encode("quoted-printable", Body, <<>>);
+    encode("quoted-printable", Body, <<>>, 0);
 encode(_, Body) ->
     Body.
 
-encode("quoted-printable", <<C, Body/binary>>, Acc) when ?IS_NONPRINTABLE(C) ->
-    H1 = util:hexdigit(C div 16),
-    H2 = util:hexdigit(C rem 16),
-    encode("quoted-printable", Body, <<Acc/binary, $=, H1, H2>>);
-encode("quoted-printable", <<C, Body/binary>>, Acc) ->
-    encode("quoted-printable", Body, <<Acc/binary, C>>);
-encode("quoted-printable", <<>>, Acc) ->
+encode("quoted-printable", Body, Acc, N) when N >= 75 ->
+    encode("quoted-printable", Body, <<Acc/binary, $=, ?CRLF>>, 0);
+encode("quoted-printable", <<?SP, C, Body/binary>>, Acc, _) when C =:= ?CR orelse C =:= ?LF ->
+    encode("quoted-printable", <<C, Body/binary>>, <<Acc/binary, "=20">>, 0);
+encode("quoted-printable", <<?TAB, C, Body/binary>>, Acc, _) when C =:= ?CR orelse C =:= ?LF ->
+    encode("quoted-printable", <<C, Body/binary>>, <<Acc/binary, "=09">>, 0);
+encode("quoted-printable", <<?CRLF, Body/binary>>, Acc, _) ->
+    encode("quoted-printable", Body, <<Acc/binary, ?CRLF>>, 0);
+encode("quoted-printable", <<C, Body/binary>>, Acc, _) when C =:= ?CR orelse C =:= ?LF ->
+    encode("quoted-printable", Body, <<Acc/binary, ?CRLF>>, 0);
+encode("quoted-printable", <<C, Body/binary>>, Acc, N) when not ?IS_PRINTABLE(C), N >= 73 ->
+    encode("quoted-printable", <<C, Body/binary>>, <<Acc/binary, $=, ?CRLF>>, 0);
+encode("quoted-printable", <<C, Body/binary>>, Acc, N) when not ?IS_PRINTABLE(C) ->
+    H1 = hexdigit(C div 16),
+    H2 = hexdigit(C rem 16),
+    encode("quoted-printable", Body, <<Acc/binary, $=, H1, H2>>, N + 3);
+encode("quoted-printable", <<C, Body/binary>>, Acc, N) ->
+    encode("quoted-printable", Body, <<Acc/binary, C>>, N + 1);
+encode("quoted-printable", <<>>, Acc, _) ->
     Acc.
 
 split_headers(Message) ->
@@ -133,12 +195,11 @@ split_headers(Message, Headers, Buffer) ->
     end.
 
 split_parts({Headers, Body}) ->
-    case content_type(Headers) of
-        {<<"multipart/",  _/binary>>, Params} ->
-            Boundary = proplists:get_value("boundary", Params),
-            clean_parts(binary:split(Body, <<"--", Boundary/binary>>, [global]), undefined, undefined, Boundary, []);
-        {_Type, _Params} ->
-            Body
+    case boundary(Headers) of
+        error ->
+            Body;
+        Boundary ->
+            clean_parts(binary:split(Body, <<"--", Boundary/binary>>, [global]), undefined, undefined, Boundary, [])
     end.
 
 clean_parts([Preamble|Parts], undefined, undefined, Boundary, Acc) ->
@@ -170,16 +231,16 @@ parse(Message) ->
 parse(header, Buffer, Line) ->
     case binary:split(<<Buffer/binary, Line/binary>>, <<":">>) of
         [Field, Value] ->
-            {lower(Field), Value};
+            {Field, Value};
         [Invalid] ->
             throw({invalid_header, Invalid})
     end.
 
-format({Headers, _} = HB) ->
-    <<(<< <<(format(header, H))/binary, ?CRLF>> || H <- Headers >>)/binary, ?CRLF, (format(body, HB))/binary>>.
+format(HB) ->
+    <<(format(headers, HB))/binary, ?CRLF, (format(body, HB))/binary>>.
 
-format(header, {Field, Value}) ->
-    <<(util:bin(Field))/binary, ":", (util:bin(Value))/binary>>;
+format(headers, {Headers, _Body}) ->
+    << <<(bin(F))/binary, ":", (bin(V))/binary, ?CRLF>> || {F, V} <- Headers >>;
 
 format(body, {_Headers, {Preamble, Parts, Epilogue, Boundary}}) ->
     <<Preamble/binary,
@@ -187,6 +248,8 @@ format(body, {_Headers, {Preamble, Parts, Epilogue, Boundary}}) ->
      "--", Boundary/binary, "--",
      Epilogue/binary>>;
 format(body, {Headers, {Parts, Boundary}}) ->
-    format(body, {Headers, {<<>>, Parts, <<?CRLF>>, Boundary}});
+    format(body, {Headers, {<<>>, Parts, <<>>, Boundary}});
+format(body, {Headers, Parts}) when is_list(Parts) ->
+    format(body, {Headers, {<<>>, Parts, <<>>, boundary(Headers)}});
 format(body, HB) ->
     iolist_to_binary(encode(HB)).
