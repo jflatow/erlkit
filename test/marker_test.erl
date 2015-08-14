c7780a811d2522945150422343005d3dc1c91491
Jared Flatow <jflatow@gmail.com>
2015-08-14 10:54:24 -0700

    log: write defaults to call form now

--- test/marker_test.erl
+++ test/marker_test.erl
@@ -7,11 +7,11 @@ marker_test() ->
     ok = path:rmrf("marker_test"),
     {ok, L} = log:open("marker_test"),
     M0 = log:marker(L, fun (I, A) -> [I|A] end, "marker"),
-    ok = util:count(fun (I, ok) -> log:write(L, util:bin(I)) end, ok, 1000),
+    {ok, _} = util:count(fun (I, _) -> log:write(L, util:bin(I)) end, {ok, undefined}, 1000),
     {M1, D1} = marker:run({M0, []}),
     ?assertMatch(1000, length(D1)),
 
-    ok = util:count(fun (I, ok) -> log:write(L, util:bin(I)) end, ok, 1000),
+    {ok, _} = util:count(fun (I, _) -> log:write(L, util:bin(I)) end, {ok, undefined}, 1000),
     {_M, D2} = marker:run({M1, D1}),
     ?assertMatch(2000, length(D2)),
 
